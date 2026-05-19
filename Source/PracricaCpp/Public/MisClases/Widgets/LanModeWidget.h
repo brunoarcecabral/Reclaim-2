fdea8613e624575abc0a117581acdd42f57ca64c
brunoarcecabral <bruno.arcecabralx@gmail.com>
2026-05-19 19:33:27 -0300

    mm

---
+++ Source/PracricaCpp/Public/MisClases/Widgets/LanModeWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LanModeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRACRICACPP_API ULanModeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
